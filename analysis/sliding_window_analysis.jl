845c4659655aa6d6795d649dd47cf45c384ca93d
LebombJames <sam.marine418@gmail.com>
2025-02-22 14:11:00 +0000

    Move CSV prep to Julia

--- analysis/sliding_window_analysis.jl
+++ analysis/sliding_window_analysis.jl
@@ -1,4 +1,4 @@
-using CSV, DataFrames, HypothesisTests, Base.Threads, Statistics
+using CSV, DataFrames, HTTP, HypothesisTests, Base.Threads, Statistics
 
 println(Threads.nthreads())
 
@@ -37,14 +37,44 @@ function filter_missings(args...)
     return collect.(skipmissings(args...))
 end
 
-function prepare_df(df, i, c, w)
+function load_csvs()
+    csvs = filter(file -> contains(file, "data_hep"), readdir("data", join = true))
+    hepdata = reduce(
+        vcat,
+        [CSV.read(filepath, DataFrame, types = Dict(:Condition => Symbol), missingstring = "NA") for filepath in csvs],
+    )
+
+    ppreq = HTTP.get(
+        "https://raw.githubusercontent.com/RealityBending/InteroceptionPrimals/main/data/data_participants.csv",
+    )
+    interoprimals = DataFrame(CSV.File(ppreq.body, missingstring = "NA"))
+    rename!(interoprimals, :participant_id => :Participant)
+
+    filtered_cols = filter(
+        col ->
+            (
+                col == "Participant" ||
+                startswith(col, "MAIA_") ||
+                startswith(col, "IAS") ||
+                startswith(col, "HRV") ||
+                startswith(col, "HCT")
+            ) && !contains(col, r"\d"),
+        names(interoprimals),
+    )
+
+    return rightjoin(hepdata, interoprimals[!, filtered_cols], on = :Participant)
+end
+
+function prepare_window(df, i, c, w)
 
     windowed = df[(df.time .> (i - w) .&& df.time .< (i + w)) .&& df.Condition .== c, :]
     grouped = groupby(windowed, :Participant)
     transform!(grouped, [:AF7, :AF8] .=> mean .=> [:AF7_Mean, :AF8_Mean])
 
     means = eachcol(parent(grouped)[!, [:AF7_Mean, :AF8_Mean]])
-    interoceptive = eachcol(select!(parent(grouped), Cols(startswith.(["MAIA", "IAS", "HRV", "HCT"]))))
+    interoceptive = eachcol(
+        select!(parent(grouped), Cols(startswith("MAIA"), startswith("IAS"), startswith("HRV"), startswith("HCT"))),
+    )
 
     return (means, interoceptive)
 end
@@ -79,13 +109,17 @@ function pairwise_correlation(means, interoceptive, i, c, w)
 end
 
 function sliding_window_analysis()
-    @time df = CSV.read("completedf.csv", DataFrame, types = Dict(:Condition => Symbol)) # "completedf.csv" is a CSV of `dffeat` from `sliding_window_analysis.qmd`, just to save me having to learn how to do that manipulation in Julia
+
+    df = load_csvs()
+    println("CSVs loaded...")
+
+    # @time df = CSV.read("completedf.csv", DataFrame, types = Dict(:Condition => Symbol, :Participant => Symbol)) # "completedf.csv" is a CSV of `dffeat` from `sliding_window_analysis.qmd`, just to save me having to learn how to do that manipulation in Julia
 
     # A w of 0.1 means 0.1 each side of the mean (total width 0.2)
     Threads.@threads for w in window_widths
         for c in conditions, i in times
 
-            (means, interoceptive) = prepare_df(df, i, c, w)
+            (means, interoceptive) = prepare_window(df, i, c, w)
 
             pairwise_correlation(means, interoceptive, i, c, w)
 
@@ -96,6 +130,6 @@ end
 
 @time sliding_window_analysis()
 
-output = filter(row -> row.n != 0, output) # Remove any uninitialised structs
+output = [output[i] for i = 1:length(output) if isassigned(output, i)] # Remove any undef
 
 finaldf = DataFrame(output)
