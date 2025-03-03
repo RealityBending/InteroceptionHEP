afdb1b6ea9a0c54c14f9a3de9773211421220826
LebombJames <sam.marine418@gmail.com>
2025-03-03 13:03:42 +0000

    Attempt at epoch analysis, not yet working

--- analysis/sliding_window_analysis.jl
+++ analysis/sliding_window_analysis.jl
@@ -2,6 +2,7 @@ using CSV, DataFrames, HTTP, HypothesisTests, Base.Threads, Statistics
 
 println(Threads.nthreads())
 
+# Types
 @kwdef struct CorTestResult
     Parameter1::Symbol = Symbol()
     Parameter2::Symbol = Symbol()
@@ -18,17 +19,22 @@ println(Threads.nthreads())
     df_error::Int = 0
 end
 
-const window_widths = range(start = 0.1, stop = 0.6, step = 0.1)
-const conditions = [:HCT, :RestingState]
-const times = range(start = -0.4, stop = 0.8, step = 0.01)
+@kwdef struct EpochCorTestResult
+    Parameter1::Symbol = Symbol()
+    Parameter2::Symbol = Symbol()
+    r::Float64 = 0.0
+    p::Float64 = 0.0
+    t::Float64 = 0.0
+    ci_lower::Float64 = 0.0
+    ci_higher::Float64 = 0.0
+    ci::Float64 = 0.0
+    n::Int = 0
+    df_error::Int = 0
+    epoch::Int = 0
+    participant::String = ""
+end
 
-const steps = length(window_widths) * length(conditions) * length(times)
-const max_corr_count = steps * 50 # 50 correlations for each combination
-
-# Preallocate the vector with a struct of dummy values
-output = Vector{CorTestResult}(undef, max_corr_count)
-
-prealloc_i::Threads.Atomic{Int} = Threads.Atomic{Int}(1) # Needed for multi-threading
+# Functions
 
 """
 Evaluate the iterator from `skipmissings` ahead of time in order to satisfy type checking.
@@ -37,7 +43,7 @@ function filter_missings(args...)
     return collect.(skipmissings(args...))
 end
 
-function load_csvs()
+function load_csvs(first::Int = nothing)
     csvs = filter(file -> contains(file, "data_hep"), readdir("data", join = true))
     hepdata = reduce(
         vcat,
@@ -47,7 +53,7 @@ function load_csvs()
     ppreq = HTTP.get(
         "https://raw.githubusercontent.com/RealityBending/InteroceptionPrimals/main/data/data_participants.csv",
     )
-    interoprimals = DataFrame(CSV.File(ppreq.body, missingstring = "NA"))
+    interoprimals = DataFrame(CSV.File(ppreq.body, missingstring = "NA"), copycols = false)
     rename!(interoprimals, :participant_id => :Participant)
 
     filtered_cols = filter(
@@ -62,74 +68,191 @@ function load_csvs()
         names(interoprimals),
     )
 
-    return rightjoin(hepdata, interoprimals[!, filtered_cols], on = :Participant)
+    joined = rightjoin(hepdata, interoprimals[!, filtered_cols], on = :Participant)
+
+    if (first isa Int)
+        return first(joined, first)
+    else
+        return joined
+    end
 end
 
-function prepare_window(df, i, c, w)
+function prepare_variables(windowed)
 
-    windowed = df[(df.time .> (i - w) .&& df.time .< (i + w)) .&& df.Condition .== c, :]
     grouped = groupby(windowed, :Participant)
-    transform!(grouped, [:AF7, :AF8] .=> mean .=> [:AF7_Mean, :AF8_Mean])
 
-    means = eachcol(parent(grouped)[!, [:AF7_Mean, :AF8_Mean]])
+    # Add the means for each participant to the respective rows, the number of rows remains the same.
+    # transformed = transform(grouped, [:AF7, :AF8] .=> mean .=> [:AF7_Mean, :AF8_Mean])
+
+    # Alternatively, create a table with one row per participant, and their associated statistics.
+    # Out of these two options, I don't know which is correct. Both fail when doing the epoch analysis.
+    combined = combine(
+        grouped,
+        [:AF7, :AF8] .=> mean .=> [:AF7_Mean, :AF8_Mean],
+        Cols(startswith("MAIA")) .=> first,
+        Cols(startswith("IAS")) .=> first,
+        Cols(startswith("HRV")) .=> first,
+        Cols(startswith("HCT")) .=> first,
+        renamecols = false,
+    )
+
+    means = eachcol(select(combined, [:AF7_Mean, :AF8_Mean], copycols = false))
+
     interoceptive = eachcol(
-        select!(parent(grouped), Cols(startswith("MAIA"), startswith("IAS"), startswith("HRV"), startswith("HCT"))),
+        select(
+            combined,
+            Cols(startswith("MAIA"), startswith("IAS"), startswith("HRV"), startswith("HCT")),
+            copycols = false,
+        ),
     )
 
     return (means, interoceptive)
 end
 
-function pairwise_correlation(means, interoceptive, i, c, w)
+function pairwise_correlation(
+    means,
+    interoceptive,
+    output,
+    prealloc_i;
+    i::Union{Float64, Nothing} = nothing,
+    c::Union{Symbol, Nothing} = nothing,
+    w::Union{Float64, Nothing} = nothing,
+    pp::Union{String7, Nothing} = nothing,
+    e::Union{Int, Nothing} = nothing,
+)
 
     for (Parameter1, cols1) in pairs(means), (Parameter2, cols2) in pairs(interoceptive) # Correlate the AF7/8 means against each interoceptive measure
 
-        cor = HypothesisTests.CorrelationTest(filter_missings(cols1, cols2)...)
+        (cols1_filtered, cols2_filtered) = filter_missings(cols1, cols2)
+
+        if ((length(cols1_filtered)) === 0 || (length(cols1_filtered)) === 0)
+            continue
+        end
+
+        cor = HypothesisTests.CorrelationTest(cols1_filtered, cols2_filtered)
+
+        if (cor.r ≈ 1.0 || cor.r ≈ -1.0 || isnan(cor.r) || cor.t ≈ Inf || cor.t ≈ -1.0 || cor.t ≈ 1.0)
+            # All the epoch correlations get caught here :(
+            continue
+        end
+
         pvalue = HypothesisTests.pvalue(cor)
         (ci_lower, ci_higher) = HypothesisTests.confint(cor)
 
-        output[prealloc_i[]] = CorTestResult(
-            Parameter1 = Parameter1,
-            Parameter2 = Parameter2,
-            r = cor.r,
-            time = i,
-            condition = c,
-            window_size = w,
-            p = pvalue,
-            t = cor.t,
-            ci_lower = ci_lower,
-            ci_higher = ci_higher,
-            ci = 0.95,
-            n = cor.n,
-            df_error = cor.n - 2, # n minus number of variables
-        )
+        if (pp === nothing && e === nothing)
 
-        atomic_add!(prealloc_i, 1)
+            output[prealloc_i[]] = CorTestResult(
+                Parameter1 = Parameter1,
+                Parameter2 = Parameter2,
+                r = cor.r,
+                time = i,
+                condition = c,
+                window_size = w,
+                p = pvalue,
+                t = cor.t,
+                ci_lower = ci_lower,
+                ci_higher = ci_higher,
+                ci = 0.95,
+                n = cor.n,
+                df_error = cor.n - 2, # n minus number of variables
+            )
+
+        else
+            output[prealloc_i[]] = EpochCorTestResult(
+                Parameter1 = Parameter1,
+                Parameter2 = Parameter2,
+                r = cor.r,
+                p = pvalue,
+                t = cor.t,
+                ci_lower = ci_lower,
+                ci_higher = ci_higher,
+                ci = 0.95,
+                n = cor.n,
+                df_error = cor.n - 2, # n minus number of variables
+                participant = pp,
+                epoch = e,
+            )
+        end
+
+        prealloc_i[] += 1
 
     end
 end
 
-function sliding_window_analysis()
+# Main
+
+function main(; epoch_analysis = false)
 
     df = load_csvs()
-    println("CSVs loaded...")
+    println("CSVs loaded.")
 
-    # @time df = CSV.read("completedf.csv", DataFrame, types = Dict(:Condition => Symbol, :Participant => Symbol)) # "completedf.csv" is a CSV of `dffeat` from `sliding_window_analysis.qmd`, just to save me having to learn how to do that manipulation in Julia
+    prealloc_i::Threads.Atomic{Int} = Threads.Atomic{Int}(1) # Needed for multi-threading
 
-    # A w of 0.1 means 0.1 each side of the mean (total width 0.2)
-    Threads.@threads for w in window_widths
-        for c in conditions, i in times
+    if (epoch_analysis)
+        epochs = filter(i -> i != 0, unique(df.epoch)) # Remove 0 because it's meaningless when used in a correlation
+        participants = unique(df.Participant)
 
-            (means, interoceptive) = prepare_window(df, i, c, w)
+        steps = length(participants) * length(epochs) * 50 # 50 correlations for each combination
 
-            pairwise_correlation(means, interoceptive, i, c, w)
+        # Preallocate the vector with a struct of dummy values
+        output = Vector{EpochCorTestResult}(undef, steps)
 
-            println("Thread $(Threads.threadid()): $i, $c, $w")
+        Threads.@threads for pp in participants
+            for e in epochs
+
+                windowed = (@view df[df.Participant .== pp .&& df.epoch .<= e, :])
+
+                if (nrow(windowed) === 0)
+                    continue
+                end
+
+                (means, interoceptive) = prepare_variables(windowed)
+
+                pairwise_correlation(means, interoceptive, output, prealloc_i; pp = pp, e = e)
+
+                println("Thread $(Threads.threadid()): $pp, $e")
+
+            end
+        end
+
+    else
+
+        window_widths = range(start = 0.1, stop = 0.6, step = 0.1)
+        conditions = [:HCT, :RestingState]
+        times = range(start = -0.4, stop = 0.8, step = 0.01)
+
+        steps = length(window_widths) * length(conditions) * length(times) * 51 # 50 correlations for each combination, plus 1 extra for good luck
+
+        # Preallocate the vector with a struct of dummy values
+        output = Vector{CorTestResult}(undef, steps)
+
+        # output = create_output_vec(steps, CorTestResult)
+
+        # A w of 0.1 means 0.1 each side of the mean (total width 0.2)
+        Threads.@threads for w in window_widths
+            for c in conditions, i in times
+
+                windowed = (@view df[(df.time .> (i - w) .&& df.time .< (i + w)) .&& df.Condition .=== c, :])
+
+                if (nrow(windowed) === 0)
+                    # println("Thread $(Threads.threadid()): SKIPPED $i, $c, $w")
+                    continue
+                end
+
+                (means, interoceptive) = prepare_variables(windowed)
+
+                pairwise_correlation(means, interoceptive, output, prealloc_i; i = i, c = c, w = w)
+
+                println("Thread $(Threads.threadid()): $i, $w, ($(i - w), $(i + w)), $c")
+            end
         end
     end
+
+    return output
 end
 
-@time sliding_window_analysis()
+@time output = main(epoch_analysis = false)
 
-output = [output[i] for i = 1:length(output) if isassigned(output, i)] # Remove any undef
+output2 = [output[i] for i = 1:length(output) if isassigned(output, i)] # Remove any undef
 
-finaldf = DataFrame(output)
+finaldf = DataFrame(output2)
